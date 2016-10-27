fe641eef22116e722f3415801f3c11abc92a15e9
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-10-27 16:43:39 -0700

    handlebar compile

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,12 +15,16 @@ Article.prototype.toHtml = function() {
   //   The result is added to the object as a new property, which can then be referenced
   //   by a key in the template. For example, you might want to display how old a post is,
   //   or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   //       - Don't forget to return your template for this article.
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  var source = $('#local-data-template').html();
+  console.log('this is ' + source);
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
 };
 
 ourLocalData.sort(function(a,b) {
