394c975d4f375c3160d30dce6ffd1152c58ccbab
Anna Ulanova <anetteua0@gmail.com>
2017-01-05 14:08:37 -0800

    first half done

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -10,17 +10,23 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // DONE: If your template will use properties that aren't on the object yet, add them.
+
+  // DONE-DONE!: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced
   //   by a key in the template. For example, you might want to display how old a post is,
   //   or say "(draft)" if it has no publication date:
+
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   // TODO: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
   //       - Don't forget to return your template for this article.
+  var source = $('#article-template').toHtml();
+  var templateRender = Handlebars.compile(source);
+  var context = articleDataSet;
+  return templateRender(this);
 };
 
 ourLocalData.sort(function(a,b) {
